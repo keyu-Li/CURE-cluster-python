1f8cd44dbda15cf94a6106b3ccf3007a51e729d4
Keyu Li <291459739@qq.com>
2022-03-01 12:44:04 +0800

    update comments

--- CURE.py
+++ CURE.py
@@ -11,57 +11,82 @@ import numpy as np
 import scipy.spatial.distance as distance
 import sys
 
-# Returns the distance between two vectors
+
+# 返回两个向量之间的距离
 def dist(vecA, vecB):
     return np.sqrt(np.power(vecA - vecB, 2).sum())
 
-# This class describes the data structure and method of operation for CURE clustering.
+
+# 此类描述 CURE 聚类的数据结构和操作方法。
 class CureCluster:
     def __init__(self, id__, center__):
+        # 该簇对应的数据集
         self.points = center__
+        # 该簇对应的代表点
         self.repPoints = center__
+        # 该簇对应的质心
         self.center = center__
+        # 该簇对应数据的索引
         self.index = [id__]
-        
+
     def __repr__(self):
         return "Cluster " + " Size: " + str(len(self.points))
-    
-    # Computes and stores the centroid of this cluster, based on its points
+
     def computeCentroid(self, clust):
+        """
+        根据其点计算并存储该集群的质心
+        :param clust: 另一个簇
+        :return:
+        """
         totalPoints_1 = len(self.index)
         totalPoints_2 = len(clust.index)
-        self.center = (self.center*totalPoints_1 + clust.center*totalPoints_2) / (totalPoints_1 + totalPoints_2)
-    
-    # Computes and stores representative points for this cluster
+        self.center = (self.center * totalPoints_1 + clust.center * totalPoints_2) / (totalPoints_1 + totalPoints_2)
+
     def generateRepPoints(self, numRepPoints, alpha):
+        """
+        计算并存储该集群的代表点
+        :param numRepPoints: 代表点的个数
+        :param alpha: 收缩因子
+        :return:
+        """
+        # 存储着已经计算出的代表点
         tempSet = None
-        for i in range(1, numRepPoints+1):
+        for i in range(1, numRepPoints + 1):
+            # 最大的距离
             maxDist = 0
+            # 最大距离对应的点
             maxPoint = None
             for p in range(0, len(self.index)):
+                # 如果计算第一个中心点，则选择距离质心最远的点作为第一个代表点
+                # 如果不是第一个点，这个点距离代表点的距离是，离这个点最近的代表点的距离
+                #     从所有点选择距离最远的点作为代表点
+                # 代表点可以重复
                 if i == 1:
-                    minDist = dist(self.points[p,:], self.center)
+                    minDist = dist(self.points[p, :], self.center)
                 else:
                     X = np.vstack([tempSet, self.points[p, :]])
                     tmpDist = distance.pdist(X)
                     minDist = tmpDist.min()
                 if minDist >= maxDist:
                     maxDist = minDist
-                    maxPoint = self.points[p,:]
+                    maxPoint = self.points[p, :]
             if tempSet is None:
                 tempSet = maxPoint
             else:
                 tempSet = np.vstack((tempSet, maxPoint))
+        # 通过alpha参数收缩并更新代表点
         for j in range(len(tempSet)):
             if self.repPoints is None:
-                self.repPoints = tempSet[j,:] + alpha * (self.center - tempSet[j,:])
+                self.repPoints = tempSet[j, :] + alpha * (self.center - tempSet[j, :])
             else:
-                self.repPoints = np.vstack((self.repPoints, tempSet[j,:] + alpha * (self.center - tempSet[j,:])))
+                self.repPoints = np.vstack((self.repPoints, tempSet[j, :] + alpha * (self.center - tempSet[j, :])))
 
-    # Computes and stores distance between this cluster and the other one.
+    # 通过簇的代表点计算并存储此簇与另一个簇之间的距离。
+    # 距离为最近的代表点的距离
     def distRep(self, clust):
         distRep = float('inf')
         for repA in self.repPoints:
+            # clust刚被初始化的时候，repPoint不是二维的
             if type(clust.repPoints[0]) != list:
                 repB = clust.repPoints
                 distTemp = dist(repA, repB)
@@ -74,57 +99,71 @@ class CureCluster:
                         distRep = distTemp
         return distRep
 
-    # Merges this cluster with the given cluster, recomputing the centroid and the representative points.
+    # 将此簇与给定簇合并，重新计算质心和代表点。
     def mergeWithCluster(self, clust, numRepPoints, alpha):
+        # 计算新的质心
         self.computeCentroid(clust)
+        # 合并数据集
         self.points = np.vstack((self.points, clust.points))
+        # 合并索引
         self.index = np.append(self.index, clust.index)
+        # 重新计算代表点
         self.repPoints = None
         self.generateRepPoints(numRepPoints, alpha)
 
-# Describe the process of the CURE algorithm
-def runCURE(data, numRepPoints, alpha, numDesCluster):
 
-    # Initialization
+def runCURE(data, numRepPoints, alpha, numDesCluster):
+    """
+    描述CURE算法的过程
+    :param data: 加载的数据集
+    :param numRepPoints: 代表点的个数
+    :param alpha: 收缩因子
+    :param numDesCluster: 期望的簇的个数
+    :return:
+    """
     Clusters = []
     numCluster = len(data)
     numPts = len(data)
+    # 初始化簇的距离矩阵，初始值为inf
     distCluster = np.ones([len(data), len(data)])
     distCluster = distCluster * float('inf')
     for idPoint in range(len(data)):
-        newClust = CureCluster(idPoint, data[idPoint,:])
+        # 为每一个点初始化类
+        newClust = CureCluster(idPoint, data[idPoint, :])
+        # 添加到Clusters列表中
         Clusters.append(newClust)
+    # 计算点之间的距离
     for row in range(0, numPts):
-    	for col in range(0, row):
-    		distCluster[row][col] = dist(Clusters[row].center, Clusters[col].center)
+        for col in range(0, row):
+            distCluster[row][col] = dist(Clusters[row].center, Clusters[col].center)
+    # 当目前簇的数量大于期望的数量，需要继续合并
     while numCluster > numDesCluster:
         if np.mod(numCluster, 50) == 0:
             print('Cluster count:', numCluster)
 
-        # Find a pair of closet clusters
+        # 找到距离最近的两个簇
         minIndex = np.where(distCluster == np.min(distCluster))
         minIndex1 = minIndex[0][0]
         minIndex2 = minIndex[1][0]
 
-        # Merge
+        # 将两个簇进行合并
         Clusters[minIndex1].mergeWithCluster(Clusters[minIndex2], numRepPoints, alpha)
-        # Update the distCluster matrix
+        # 更新距离矩阵
         for i in range(0, minIndex1):
             distCluster[minIndex1, i] = Clusters[minIndex1].distRep(Clusters[i])
-        for i in range(minIndex1+1, numCluster):
+        for i in range(minIndex1 + 1, numCluster):
             distCluster[i, minIndex1] = Clusters[minIndex1].distRep(Clusters[i])
-        # Delete the merged cluster and its disCluster vector.
+        # 删除被合并簇所在的行和列
         distCluster = np.delete(distCluster, minIndex2, axis=0)
         distCluster = np.delete(distCluster, minIndex2, axis=1)
         del Clusters[minIndex2]
         numCluster = numCluster - 1
 
     print('Cluster count:', numCluster)
-    # Generate sample labels
+    # 生成label
     Label = [0] * numPts
     for i in range(0, len(Clusters)):
         for j in range(0, len(Clusters[i].index)):
             Label[Clusters[i].index[j]] = i + 1
 
     return Label
-
--- test_cure.py
+++ test_cure.py
@@ -1,31 +1,31 @@
 # -*- coding:utf-8 -*-
 
 from CURE import *
-import sys,time
+import sys, time
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import metrics
 
-# The number of representative points
+# 代表点的数量
 numRepPoints = 5
-# Shrink factor
+# 收缩因子
 alpha = 0.1
-# Desired cluster number
-numDesCluster = 3
+# 期望的簇的个数
+numDesCluster = 15
 
 start = time.clock()
-data_set = np.loadtxt('./3clus.txt')
-data = data_set[:,0:2]
-Label_true = data_set[:,2]
-print("Please wait for CURE clustering to accomplete...")
+data_set = np.loadtxt('./R15.txt')
+data = data_set[:, 0:2]
+Label_true = data_set[:, 2]
+print("等待CURE聚类完成...")
 Label_pre = runCURE(data, numRepPoints, alpha, numDesCluster)
-print("The CURE clustering is accompleted!!\n")
+print("CURE完成聚类!!\n")
 end = time.clock()
-print("The time of CURE algorithm is", end - start, "\n")
-# Compute the NMI
+print("用时：", end - start, "\n")
+# 计算NMI
 nmi = metrics.v_measure_score(Label_true, Label_pre)
 print("NMI =", nmi)
-# Plot the result
+# 绘制结果
 plt.subplot(121)
 plt.scatter(data_set[:, 0], data_set[:, 1], marker='.')
 plt.text(0, 0, "origin")
@@ -33,8 +33,8 @@ plt.subplot(122)
 scatterColors = ['black', 'blue', 'green', 'yellow', 'red', 'purple', 'orange', 'brown', 'cyan', 'brown',
                  'chocolate', 'darkgreen', 'darkblue', 'azure', 'bisque']
 for i in range(data_set.shape[0]):
-    color = scatterColors[Label_pre[i]-1]
+    color = scatterColors[Label_pre[i] - 1]
     plt.scatter(data_set[i, 0], data_set[i, 1], marker='o', c=color)
 plt.text(0, 0, "clusterResult")
 plt.show()
-
+# plt.savefig("Figure_1.png")
